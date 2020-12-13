f99fe5dfb72384be88512c63ad055866189eaff6
njdeane <njdeane7@gmail.com>
2020-12-13 16:03:41 +1000

    View complete with auto layout, IBA & IBO

--- RockPaperScissors/Controller/ViewController.swift
+++ RockPaperScissors/Controller/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageViewLeft: UIImageView!
+    @IBOutlet weak var imageViewRight: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func goButton(_ sender: UIButton) {
+    }
+    
 }
 
